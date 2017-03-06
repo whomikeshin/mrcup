970a4e6367f38e64859aa57ad74fad67a53b4faf
Mike Shin <michaelkshin@gmail.com>
2017-03-06 13:36:21 -0500

    Add methods to user model
    add validations, password generation, and session methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,36 @@
 class User < ApplicationRecord
-  
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  validates :username, :email, :session_token, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password, length: { minimum: 6 }
+  validates :username, :email, :password_digest, :session_token, presence: true
+
+  # add in find by email option
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
