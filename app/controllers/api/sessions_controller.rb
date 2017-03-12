41e437ddadfe848410ee079aaa4669c6f6cdccec
Mike Shin <michaelkshin@gmail.com>
2017-03-12 15:33:37 -0400

    Add api sessions and users controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  class SessionsController < ApplicationController
+    def create
+      @user = User.find_by_credentials(
+        params[:username],
+        params[:password]
+      )
+
+      if @user
+        sign_in(@user)
+        render "api/users/show"
+      else
+        render json: ["Invalid username or password"], status: 401
+      end
+    end
+
+    def show
+      if current_user
+        @user = current_user
+        render "api/users/show"
+      else
+        render json: {}
+      end
+    end
+
+    def destroy
+      sign_out
+      render json: {}
+    end
+  end
+end
