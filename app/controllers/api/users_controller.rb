88d2a18974bfcc9daca51a8db0b779cf646f65df
Mike Shin <michaelkshin@gmail.com>
2017-03-13 12:01:04 -0400

    Add api controllers, root file
    ignore tmp folder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,35 +1,32 @@
-module Api
-  class UserController < ApplicationController
-    before_action :require_signed_in!, only: [:update]
+class Api::UsersController < ApplicationController
+  before_action :require_signed_in!, only: [:update]
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        sign_in(@user)
-        # render "api/tracks/index"
-        render :show
-      else
-        render json: @user.errors.full_messages, status: 422
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
     end
+  end
 
-    def update
-      @user = current_user
+  def update
+    @user = current_user
 
-      if @user.update(user_params)
-        render :show
-      else
-        render json: @user.errors.full_messages, status: 422
-      end
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    private
-    def user_params
-      params.require(:user).permit(:email, :username, :password)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end
