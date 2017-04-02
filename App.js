8b0fa0d9f3abaf699745e696bff86bff7bda8ebc
Mike Shin <michaelkshin@gmail.com>
2017-04-02 19:55:16 -0400

    Add StackNavigator and TabNavigator to App.js

--- App.js
+++ App.js
@@ -5,44 +5,76 @@ import {
   View,
   Button,
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 
-class HomeScreen extends React.Component {
+import {
+  StackNavigator,
+  TabNavigator,
+} from 'react-navigation';
+
+import UserAuth from './app/components/userauth';
+
+class ChatScreen extends React.Component {
   static navigationOptions = {
-    title: 'Welcome',
+    // Nav options can be defined as a function of the navigation prop:
+    // what does `` do?
+    title: ({ state }) => `Chat with ${state.params.user}`,
   };
+  render() {
+    // The screen's current route is passed in to `props.navigation.state`:
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with {params.user}</Text>
+      </View>
+    );
+  }
+}
+
+class RecentChatsScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>Hello, Chat App!</Text>
-        <Button
-          onPress={() => navigate('Chat')}
-          title="Chat with Lucy"
-        />
+        <Text>List of recent chats</Text>
+          <Button
+            onPress={() => navigate('Chat', { user: 'Lucy' })}
+            title="Chat with Lucy"
+          />
       </View>
     );
   }
 }
 
-class ChatScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Chat with Lucy',
-  };
+class AllContactsScreen extends React.Component {
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>Chat with Lucy</Text>
+        <Text>List of all contacts</Text>
+          <Button
+            onPress={() => navigate('Login')}
+            title="Login"
+          />
       </View>
     );
   }
 }
+
+const MainScreenNavigator = TabNavigator({
+  Recent: { screen: RecentChatsScreen },
+  All: { screen: AllContactsScreen},
+});
+
+MainScreenNavigator.navigationOptions = {
+  title: 'My Chats',
+};
 
 // case sensitive must be mrcup
 // react-native init mrcup
 const mrcup = StackNavigator({
-  Home: { screen: HomeScreen },
+  Home: { screen: MainScreenNavigator },
   Chat: { screen: ChatScreen },
+  Login: { screen: UserAuth },
 });
 
 AppRegistry.registerComponent('mrcup', () => mrcup);
