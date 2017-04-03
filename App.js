3c6526a62745a94afa73aa48a4db30c831800209
Mike Shin <michaelkshin@gmail.com>
2017-04-02 21:33:15 -0400

    Modify component names and html content

--- App.js
+++ App.js
@@ -13,18 +13,18 @@ import {
 
 import UserAuth from './app/components/userauth';
 
-class ChatScreen extends React.Component {
+class SessionScreen extends React.Component {
   static navigationOptions = {
     title: ({ state }) => {
       if (state.params.mode === 'info') {
-        return `${state.params.user}'s Contact Info`;
+        return 'Advanced';
       }
-      return `Chat with ${state.params.user}`;
+      return `${state.params.name}`;
     },
     header: ({ state, setParams }) => {
       let right = (
         <Button
-          title={`${state.params.user}'s info`}
+          title='Advanced'
           onPress={() => setParams({ mode: 'info' })}
         />
       );
@@ -39,39 +39,39 @@ class ChatScreen extends React.Component {
       return { right };
     },
   };
-  
+
   render() {
     // The screen's current route is passed in to `props.navigation.state`:
     const { params } = this.props.navigation.state;
     return (
       <View>
-        <Text>Chat with {params.user}</Text>
+        <Text>{params.name} cupping session</Text>
       </View>
     );
   }
 }
 
-class RecentChatsScreen extends React.Component {
+class StartScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>List of recent chats</Text>
+        <Text>Start Cupping</Text>
           <Button
-            onPress={() => navigate('Chat', { user: 'Lucy' })}
-            title="Chat with Lucy"
+            onPress={() => navigate('Session', { name: 'Arabica' })}
+            title="Session"
           />
       </View>
     );
   }
 }
 
-class AllContactsScreen extends React.Component {
+class AccountScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Text>List of all contacts</Text>
+        <Text>Account Info</Text>
           <Button
             onPress={() => navigate('Login')}
             title="Login"
@@ -82,19 +82,17 @@ class AllContactsScreen extends React.Component {
 }
 
 const MainScreenNavigator = TabNavigator({
-  Recent: { screen: RecentChatsScreen },
-  All: { screen: AllContactsScreen},
+  Start: { screen: StartScreen },
+  Account: { screen: AccountScreen},
 });
 
 MainScreenNavigator.navigationOptions = {
-  title: 'My Chats',
+  title: 'CUFFEE',
 };
 
-// case sensitive must be mrcup
-// react-native init mrcup
 const mrcup = StackNavigator({
   Home: { screen: MainScreenNavigator },
-  Chat: { screen: ChatScreen },
+  Session: { screen: SessionScreen },
   Login: { screen: UserAuth },
 });
 
