103660b4c241f2e46a800a2a010ba1368ca257df
Mike Shin <michaelkshin@gmail.com>
2017-04-02 20:09:20 -0400

    Add header prop to navigationOptions

--- App.js
+++ App.js
@@ -15,10 +15,31 @@ import UserAuth from './app/components/userauth';
 
 class ChatScreen extends React.Component {
   static navigationOptions = {
-    // Nav options can be defined as a function of the navigation prop:
-    // what does `` do?
-    title: ({ state }) => `Chat with ${state.params.user}`,
+    title: ({ state }) => {
+      if (state.params.mode === 'info') {
+        return `${state.params.user}'s Contact Info`;
+      }
+      return `Chat with ${state.params.user}`;
+    },
+    header: ({ state, setParams }) => {
+      let right = (
+        <Button
+          title={`${state.params.user}'s info`}
+          onPress={() => setParams({ mode: 'info' })}
+        />
+      );
+      if (state.params.mode === 'info') {
+        right = (
+          <Button
+            title="Done"
+            onPress={() => setParams({ mode: 'none' })}
+          />
+        );
+      }
+      return { right };
+    },
   };
+  
   render() {
     // The screen's current route is passed in to `props.navigation.state`:
     const { params } = this.props.navigation.state;
