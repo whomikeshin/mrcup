0bca6ff53ed34b8cd363439ac236e45a68b45ee7
Mike Shin <michaelkshin@gmail.com>
2017-08-21 16:05:40 -0400

    Add blind switch component

--- frontend/components/advanced.js
+++ frontend/components/advanced.js
@@ -8,21 +8,39 @@ import {
   View,
   TextInput,
   Button,
-  Alert,
+  Switch,
 } from 'react-native';
 
 export default class Advanced extends Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { params } = this.props.navigation.state;
+    this.state = {
+      sampleCount: params.sampleCount,
+      cupCount: params.cupCount,
+      blind: false,
+    };
+  }
+
+  render() {
     return (
       <View>
-        <Text>Samples: {params.sampleCount}</Text>
-        <Text>Cups: {params.cupCount}</Text>
+        <Text>Samples: {this.state.sampleCount}</Text>
+        <Text>Cups: {this.state.cupCount}</Text>
+        <Text>Blind?</Text>
+        <Switch
+          onValueChange={(value) => this.setState({blind: value})}
+          value={this.state.blind} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 10,
+  },
 });
