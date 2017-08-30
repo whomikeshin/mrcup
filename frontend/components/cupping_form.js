9e0a8c7cc069fbad697ceaacac283af7c4142cd5
Mike Shin <michaelkshin@gmail.com>
2017-08-30 17:16:59 -0400

    Add SectionList example

--- frontend/components/cupping_form.js
+++ frontend/components/cupping_form.js
@@ -11,6 +11,9 @@ import {
   Alert,
   ScrollView,
   Slider,
+  SectionList,
+  ListItem,
+  FlatList
 } from 'react-native';
 
 export default class CuppingForm extends Component {
@@ -22,49 +25,22 @@ export default class CuppingForm extends Component {
       fragrance: 6,
       dry: 0,
       break: 0,
+      data: [{'fragrance': 6}, {'dry': 0}, {'break': 0}]
     };
   }
 
   render() {
-    const { navigate } = this.props.navigation;
+    // const { navigate } = this.props.navigation;
     return (
-      <ScrollView>
-        <Text style={styles.header}>
-          Sample Name
-        </Text>
-        <View style={styles.div}>
-          <Text>Fragrance/Aroma</Text>
-          <Text>{this.state.fragrance}</Text>
-          <Slider
-            style={styles.slider}
-            maximumValue={10}
-            minimumValue={6}
-            step={.25}
-            onValueChange={(value) => this.setState({fragrance: value})}>
-          </Slider>
-          <Text>Dry</Text>
-          <Text>{this.state.dry}</Text>
-          <Slider
-            style={styles.slider}
-            maximumValue={5}
-            minimumValue={0}
-            step={.25}
-            onValueChange={(value) => this.setState({dry: value})}>
-          </Slider>
-          <Text>Break</Text>
-          <Text>{this.state.break}</Text>
-          <Slider
-            style={styles.slider}
-            maximumValue={5}
-            minimumValue={0}
-            step={.25}
-            onValueChange={(value) => this.setState({break: value})}>
-          </Slider>
-        </View>
-        <View style={styles.div}>
-
-        </View>
-      </ScrollView>
+      <SectionList
+        renderItem={({item}) => <ListItem title={item.title} />}
+        renderSectionHeader={({section}) => <h1 title={section.title} />}
+        sections={[ // homogenous rendering between sections
+          {data: [6], title: 'fragrance'},
+          {data: [0], title: 'dry'},
+          {data: [0], title: 'break'},
+        ]}
+      />
     );
   }
 }
@@ -84,3 +60,41 @@ const styles = StyleSheet.create({
     width: 300,
   }
 });
+
+// <ScrollView>
+//   <Text style={styles.header}>
+//     Sample Name
+//   </Text>
+//   <View style={styles.div}>
+//     <Text>Fragrance/Aroma</Text>
+//     <Text>{this.state.fragrance}</Text>
+//     <Slider
+//       style={styles.slider}
+//       maximumValue={10}
+//       minimumValue={6}
+//       step={.25}
+//       onValueChange={(value) => this.setState({fragrance: value})}>
+//     </Slider>
+//     <Text>Dry</Text>
+//     <Text>{this.state.dry}</Text>
+//     <Slider
+//       style={styles.slider}
+//       maximumValue={5}
+//       minimumValue={0}
+//       step={.25}
+//       onValueChange={(value) => this.setState({dry: value})}>
+//     </Slider>
+//     <Text>Break</Text>
+//     <Text>{this.state.break}</Text>
+//     <Slider
+//       style={styles.slider}
+//       maximumValue={5}
+//       minimumValue={0}
+//       step={.25}
+//       onValueChange={(value) => this.setState({break: value})}>
+//     </Slider>
+//   </View>
+//   <View style={styles.div}>
+//
+//   </View>
+// </ScrollView>
